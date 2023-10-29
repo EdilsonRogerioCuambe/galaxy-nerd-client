506f8294d7e617c085ae1c962043028f0a8ad548
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2023-10-29 04:50:48 -0300

    student card profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -14,35 +14,46 @@ export function StudentProfile() {
       <div className="text-[#c4c4cc] max-w-7xl mx-auto">
         <div className="flex flex-col md:flex-row">
           <div className="md:w-1/3 p-4">
-            <div className="bg-secondary p-4 rounded-lg">
+            <div className="bg-secondary p-6 rounded-lg relative">
+              <img
+                src={studentData?.student?.banner}
+                alt={studentData?.student?.name}
+                className="w-full h-40 object-cover rounded-lg"
+              />
               <img
                 src={studentData?.student?.avatar}
                 alt={studentData?.student?.name}
-                className="w-32 h-32 rounded-lg mx-auto object-cover"
+                className="w-24 h-24 object-cover rounded-lg border-4 border-purple-800 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2"
               />
-              <h2 className="text-xl font-bold mt-4">
-                {studentData?.student?.name}
-              </h2>
-              <p>{studentData?.student?.description}</p>
-              <p>{studentData?.student?.location}</p>
-              <p className="text-sm">
-                Email:
-                {studentData?.student?.email}
-              </p>
-              <p>
-                Home address: 92 Miles Drive, Newark, NJ 07103, California,
-                United States of America
-              </p>
-              <p>Phone number: +00 123 456 789 / +12 345 678</p>
-              <h4 className="text-md font-bold">Interesses</h4>
-              <div className="flex">
+              <div className="mt-14 text-center text-[#c4c4cc] prose prose-h2:text-[#c4c4cc]">
+                <h2 className="text-xl font-bold mb-2">
+                  {studentData?.student?.name}
+                </h2>
+                <p className="mb-2 text-red-400 font-extrabold text-lg">
+                  {studentData?.student?.profession}
+                </p>
+                <p className="mb-2">
+                  <span className="font-extrabold">Email:</span>{' '}
+                  {studentData?.student?.email}
+                </p>
+                <p className="mb-2">
+                  <span className="font-extrabold">Endereço:</span>{' '}
+                  {studentData?.student?.location}
+                </p>
+                <p className="mb-4">
+                  <span className="font-extrabold">Telefone:</span>{' '}
+                  {studentData?.student?.phone}
+                </p>
+              </div>
+              <h4 className="text-lg font-bold mb-2">Interesses</h4>
+              <div className="flex flex-wrap gap-2">
                 {studentData?.student?.interests?.map(
                   ({ icon, name }: { icon: string; name: string }) => (
                     <img
                       key={name}
                       src={icon}
                       alt={name}
-                      className="w-8 h-8 mr-2"
+                      className="w-10 h-10"
                     />
                   ),
                 )}
@@ -50,13 +61,13 @@ export function StudentProfile() {
             </div>
 
             {/* Habilidades do estudante */}
-            <div className="mt-4">
+            <div className="mt-4 bg-secondary p-4 rounded-lg">
               <h2 className="text-lg font-bold">Skills</h2>
-              <ul>
-                <li>Front-end Development</li>
-                <li>UI/UX Design</li>
-                {/* Adicione mais habilidades conforme necessário */}
-              </ul>
+              <div className="flex flex-wrap gap-2">
+                {studentData?.student?.skills?.map((url: string) => (
+                  <img src={url} alt="skills" key={url} className="w-10 h-10" />
+                ))}
+              </div>
             </div>
 
             {/* Hobbies */}
